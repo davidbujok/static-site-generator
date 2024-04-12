8dbcd91b78fdada7167586d7ceff2ea93f301a4e
davidbujok <davidbujok01@gmail.com>
2024-04-12 17:26:32 +0100

    Leaf node class implementation

---
+++ src/leafnode.py
@@ -0,0 +1,28 @@
+from htmlnode import HTMLNode
+class LeafNode(HTMLNode):
+
+    def __init__(self, value, tag=None, props=None):
+
+        super().__init__(value, tag, None, props)
+
+    def __repr__(self):
+        return f"LeafNode: {self.tag} {self.value} {self.props}"
+
+    def to_html(self):
+        if self.value is None:
+            return ValueError
+        if self.tag is None:
+            return self.value
+
+        if self.props is not None:
+            attributes = self.props_to_html()
+            new_tag = f"<{self.tag}{attributes}>{self.value}</{self.tag}>"
+            return new_tag
+        else:
+            new_tag = f"<{self.tag}>{self.value}</{self.tag}>"
+            return new_tag
+
+
+
+    
+        
