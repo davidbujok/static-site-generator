6d6a327a5971c02c5062e2b9318478d5d8c3e1e6
davidbujok <davidbujok01@gmail.com>
2024-04-12 10:08:25 +0100

    Equality method to compare instances values

--- src/textnode.py
+++ src/textnode.py
@@ -1,13 +1,18 @@
 class Textnode:
 
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
 
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def is_equal(self, other):
-        return self.__eq__(other)
+    def __eq__(self, other):
+        return(
+                self.text_type == other.text_type and
+                self.text == other.text and
+                self.url == other.url
+        )
+        
 
     def __repr__(self) -> str:
         return f"Text: {self.text} Type: {self.text_type} Url:{self.url}"
