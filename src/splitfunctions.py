df68fee30fad789167e784bc43db77f0e52b61a8
davidbujok <davidbujok01@gmail.com>
2024-05-03 18:24:05 +0100

    Refactor function to work with random order
    Text_to_textnode function now working with plain
    text that introduce multiple nodes in any
    order

--- src/splitfunctions.py
+++ src/splitfunctions.py
@@ -1,18 +1,21 @@
-import re
+import itertools
 from functions import extract_markdown_images, extract_markdown_links
 from textnode import TextType, Textnode
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type): 
 
+def split_node_delimiter(node, delimiter, text_type):
+
+    new_nodes = []
     if text_type != TextType.TEXT:
         pass
 
-    copy_text = old_nodes.text
+    copy_text = node.text
+    if delimiter not in copy_text:
+        return [node]
+
     split_text = copy_text.split(delimiter)
     remove_empty_items = filter(lambda x: len(x) != 0, split_text)
     text_list = list(remove_empty_items)
-    new_nodes = []
-
     for index, text in enumerate(text_list):
         if index % 2 == 0:
             node = Textnode(text, TextType.TEXT)
@@ -22,6 +25,16 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
 
     return new_nodes
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    processed_nodes = []
+    for node in old_nodes:
+        new_nodes = split_node_delimiter(node, delimiter, text_type)
+        if len(new_nodes):
+            processed_nodes.append(new_nodes)
+    return processed_nodes
+
 
 def split_node_images(old_nodes):
     if type(old_nodes) == str:
@@ -35,7 +48,7 @@ def split_node_images(old_nodes):
     prev_image = ""
     for node in nodes_copy:
         images = extract_markdown_images(node.text)
-        if images is None:
+        if not len(images):
             new_nodes.append(node)
             continue
         for image in images:
@@ -52,6 +65,7 @@ def split_node_images(old_nodes):
             prev_image = f"![{image[0]}]({image[1]})"
     return new_nodes
 
+
 def split_node_links(old_nodes):
     if type(old_nodes) == str:
         old_nodes = Textnode(old_nodes, TextType.TEXT)
@@ -64,7 +78,7 @@ def split_node_links(old_nodes):
     prev_link = ""
     for node in nodes_copy:
         links = extract_markdown_links(node.text)
-        if links is None:
+        if not len(links):
             new_nodes.append(node)
             continue
         for link in links:
@@ -81,37 +95,19 @@ def split_node_links(old_nodes):
             prev_link = f"[{link[0]}]({link[1]})"
     return new_nodes
 
+
 def text_to_textnodes(text):
-    extracted_nodes = []
-    text = Textnode(text, TextType.TEXT)
+
+    text = [Textnode(text, TextType.TEXT)]
 
     bold_texts = split_nodes_delimiter(text, "**", TextType.BOLD)
-    if len(bold_texts) > 0:
-        for index in range(len(bold_texts) - 1):
-            extracted_nodes.append(bold_texts[index])
+    italic_texts = split_nodes_delimiter(itertools.chain(*bold_texts), "*", TextType.ITALIC)
+    code_blocks = split_nodes_delimiter(itertools.chain(*italic_texts), "`", TextType.CODE)
+    images = split_node_images(itertools.chain(*code_blocks))
+    nodes_to_return = split_node_links(images)
 
-    italic_texts = split_nodes_delimiter([bold_texts[0], bold_texts[-1]], "*", TextType.ITALIC)
-    if len(italic_texts) > 0:
-        for index in range(len(italic_texts) - 1):
-            extracted_nodes.append(bold_texts[index])
+    return list(nodes_to_return)
 
-    code_blocks = split_nodes_delimiter(italic_texts[-1], "`", TextType.CODE)
-    if len(code_blocks) > 0:
-        for index in range(len(code_blocks) - 1):
-            extracted_nodes.append(code_blocks[index])
-
-    images = split_node_images(code_blocks[-1])
-    if len(images) > 0:
-        for index in range(len(images) - 1):
-            extracted_nodes.append(images[index])
-
-    if len(images) > 0:
-        links = split_node_links(images[-1])
-        if len(links) > 0:
-            for index in range(len(links)):
-                extracted_nodes.append(links[index])
-    return extracted_nodes
-    
 def markdown_to_block(markdown):
     lines = markdown.split("\n")
     block = []
@@ -125,4 +121,3 @@ def markdown_to_block(markdown):
     if len(block) > 0:
         blocks.append(block)
     return blocks
-
